0f4f60d111f0e3f4995bb557b710ed6bd26e7b52
Bruce Melo <brucemelodf@gmail.com>
2023-09-08 14:08:51 -0300

    Create dev-install.sh

---
+++ dev-install.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt install vim -y
+sudo apt install curl -y
+sudo apt install chromium-browser -y
+sudo apt install git -y
+
+# sdkman
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+# nodejs (nvm)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install --lts
+
+# docker
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg 
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce
+sudo usermod -aG docker ${USER}
+su - ${USER}
+echo "Dev install finished!"
